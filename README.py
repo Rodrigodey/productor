4d3bc7a36c0191298615378010e7dd75160d4468
Rodrigodey <133475744+Rodrigodey@users.noreply.github.com>
2023-05-12 23:21:40 -0500

    README.py

---
+++ README.py
@@ -0,0 +1,21 @@
+# productor
+prodructor-consumidor memoria compartida
+import multiprocessing
+
+def productor(cola):
+    for i in range(10):
+        cola.put(i)
+        print(f"El productor agregó {i} a la cola")
+
+def consumidor(cola):
+    while True:
+        item = cola.get()
+        print(f"El consumidor eliminó {item} de la cola")
+
+if __name__ == "__main__":
+    cola = multiprocessing.Queue()
+    productor_process = multiprocessing.Process(target=productor, args=(cola,))
+    consumidor_process = multiprocessing.Process(target=consumidor, args=(cola,))
+    productor_process.start()
+    consumidor_process.start()
+    productor_process.join()
